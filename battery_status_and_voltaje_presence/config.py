900139a778a57befcea7fcdc5011c679815650f1
EmilioDifferding <emiliodiffer@gmail.com>
2021-03-27 18:33:19 -0300

    config file and requirements.txt

---
+++ battery_status_and_voltaje_presence/config.py
@@ -0,0 +1,9 @@
+# WIFI SSID and Password
+WIFI_SSID = "your_wifi"
+WIFI_PASSWORD = "yourWifiPass"
+
+SLEEP_INTERVAL = 6000 #interval for deep Sleep mode in seconds
+
+# Input pins
+BATTERY = 0
+VOLTAJE_STATUS = 5
